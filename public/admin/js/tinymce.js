59af7e1ec38c7a1b427fc8eaeccb13c9b9297a36
Cao-nguyen <chinhphucnguvan@gmail.com>
2024-07-31 15:06:08 +0700

    0

--- public/admin/js/tinymce.js
+++ public/admin/js/tinymce.js
@@ -5,12 +5,12 @@ tinymce.init({
     plugins: 'quote codesample advlist autolink lists link image charmap print preview anchor searchreplace visualblocks code fullscreen insertdatetime media table paste help wordcount',
     toolbar: 'bold italic backcolor quote | formatselect | image table | link codesample | alignleft aligncenter alignright alignjustify | bullist numlist outdent indent | removeformat | undo redo',
     menubar: false,
-    // setup: function(editor) {
-    //     editor.ui.registry.addButton('quote', {
-    //         icon: 'quote',
-    //         onAction: function () {
-    //             editor.insertContent('<blockquote>Your quote here</blockquote>');
-    //         }
-    //     });
-    // }
-});  
+    setup: function(editor) {
+        editor.ui.registry.addButton('quote', {
+            icon: 'quote',
+            onAction: function () {
+                editor.insertContent('<blockquote>Your quote here</blockquote>');
+            }
+        })
+    }
+});
