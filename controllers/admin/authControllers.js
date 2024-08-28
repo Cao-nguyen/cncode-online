d077ac585e1b8c9d51d1b656fe69c39dcd367ab3
Cao-nguyen <chinhphucnguvan@gmail.com>
2024-08-28 10:44:01 +0700

    o

--- controllers/admin/authControllers.js
+++ controllers/admin/authControllers.js
@@ -1,6 +1,40 @@
+const md5 = require('md5')
+const Accounts = require('../../models/accountsModel')
+
 //[GET] /admin/auth/login
-module.exports.login = (req, res) => {
-    res.render('admin/pages/auth/login'), {
-        pageTitle: 'Đăng nhập'
+module.exports.login = async (req, res, next) => {
+    res.render('admin/pages/auth/login', { 
+        pageTitle: 'Đăng nhập',
+    });
+}
+
+//[POST] /admin/auth/login
+module.exports.loginPost = async (req, res, next) => {
+    const email = req.body.email
+    const password = req.body.password
+
+    const user = await Accounts.findOne({
+        email: email,
+        deleted: false
+    })
+
+    if(!user) {
+        req.flash('error', 'Email không tồn tại')
+        res.redirect('back')
+        return
+    } 
+
+    if(md5(password) != user.password) {
+        req.flash('error', 'Mật khẩu không chính xác')
+        res.redirect('back')
+        return
     }
+
+    if(user.status != "active") {
+        req.flash('error', 'Tài khoản đã bị khoá')
+        res.redirect('back')
+        return
+    }
+
+    res.redirect("/admin/dashboard")
 }
--- routes/admin/authRoute.js
+++ routes/admin/authRoute.js
@@ -4,5 +4,6 @@ const router = express.Router();
 const controllers = require('../../controllers/admin/authControllers');
 
 router.get('/login', controllers.login);
+router.post('/login', controllers.loginPost);
 
 module.exports = router;
