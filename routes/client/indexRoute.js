fd72aa49ce14da40cbbc50bdcb6b2e1d19124d6b
Cao-nguyen <chinhphucnguvan@gmail.com>
2024-08-31 21:15:11 +0700

    o

--- routes/client/indexRoute.js
+++ routes/client/indexRoute.js
@@ -2,8 +2,10 @@ const homeRoutes = require('./homeRoute')
 const productsRoutes = require('./productsRoute')
 const search = require('./searchRoute')
 const user = require('./userRoute')
+const userMiddleware = require('../../middlewares/client/user')
 
 module.exports = (app) => {
+    app.use(userMiddleware.inforUser)
     app.use('/', homeRoutes)
     app.use('/products', productsRoutes)
     app.use('/search', search)
