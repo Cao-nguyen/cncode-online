30c8136a1f9fee65c5a26f60703b3521217bdfe1
Cao-nguyen <chinhphucnguvan@gmail.com>
2024-08-31 21:23:32 +0700

    o

--- routes/client/userRoute.js
+++ routes/client/userRoute.js
@@ -7,5 +7,6 @@ router.get('/register', controllers.user)
 router.post('/register', controllers.userPost)
 router.get('/login', controllers.login)
 router.post('/login', controllers.loginPost)
+router.get('/logout', controllers.logout)
 
 module.exports = router
