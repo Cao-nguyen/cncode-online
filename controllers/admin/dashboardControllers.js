cb4fc0f89259cdb72f36fe10986689c6ff0947ad
Cao-nguyen <chinhphucnguvan@gmail.com>
2024-09-02 21:50:57 +0700

    o

--- controllers/admin/dashboardControllers.js
+++ controllers/admin/dashboardControllers.js
@@ -1,9 +1,67 @@
-const { listmongoose } = require('../../util/mongoose');
-const { onemongoose } = require('../../util/mongoose')
+const Category = require('../../models/categoryModel')
+const Product = require('../../models/productsModel')
+const Account = require('../../models/accountsModel')
+const User = require('../../models/usersModel')
 
 // [GET] /admin/dashboard
 module.exports.dashboard = async (req, res, next) => {
+    const statistc = {
+        category: {
+            total: 0
+        },
+        products: {
+            total: 0,
+        },
+        account: {
+            total: 0,
+            active: 0,
+            inactive: 0
+        },
+        user: {
+            total: 0,
+            active: 0,
+            inactive: 0
+        }
+    }
+
+    statistc.category.total = await Category.count({
+        deleted: false
+    })
+
+    statistc.products.total = await Product.count({
+        deleted: false
+    })
+
+    statistc.account.total = await Account.count({
+        deleted: false
+    })
+
+    statistc.user.total = await User.count({
+        deleted: false
+    })
+
+    statistc.account.active = await Account.count({
+        deleted: false,
+        status: active
+    })
+
+    statistc.user.active = await User.count({
+        deleted: false,
+        status: active
+    })
+
+    statistc.account.inactive = await Account.count({
+        deleted: false,
+        status: inactive
+    })
+
+    statistc.user.inactive = await User.count({
+        deleted: false,
+        status: inactive
+    })
+
     res.render('admin/pages/dashboard/index', { 
-        pageTitle: 'Tổng quan'
+        pageTitle: 'Tổng quan',
+        statistc: statistc
     })
 }
--- routes/client/indexRoute.js
+++ routes/client/indexRoute.js
@@ -3,9 +3,11 @@ const productsRoutes = require('./productsRoute')
 const search = require('./searchRoute')
 const user = require('./userRoute')
 const userMiddleware = require('../../middlewares/client/user')
+const settingsGeneralMiddleware = require('../../middlewares/client/settingsGeneral')
 
 module.exports = (app) => {
     app.use(userMiddleware.inforUser)
+    app.use(settingsGeneralMiddleware.settingsGeneral)
     app.use('/', homeRoutes)
     app.use('/products', productsRoutes)
     app.use('/search', search)
