d603eca8ea93c115ba9a70ef3b92d81180a1af7c
Cao-nguyen <chinhphucnguvan@gmail.com>
2024-09-04 20:08:23 +0700

    chat

--- public/js/chat.js
+++ public/js/chat.js
@@ -1,5 +1,5 @@
 // CLIENT_SEND_MESSAGE
-const formSendData = document.querySelector(".messages .message-input");
+const formSendData = document.querySelector(".chat-area .message-input");
 
 if (formSendData) {
     formSendData.addEventListener('submit', (e) => {
@@ -18,11 +18,20 @@ socket.on("SERVER_RETURN_MESSAGE", (data) => {
     const body = document.querySelector(".chat-area .messages")
 
     const div = document.createElement("div") 
-    div.classList.add("message.received")
+    div.classList.add("message", "received")
     div.innerHTML = `
         <div class="message-content">${data.fullName}</div>
         <div class="message-chat">${data.content}</div>
     `
 
     body.appendChild(div)
+
+    bodyChat.scrollTop = bodyChat.scrollHeight
 })
+
+// Scroll Chat 
+const bodyChat = document.querySelector(".chat-area .messages")
+
+if(bodyChat) {
+    bodyChat.scrollTop = bodyChat.scrollHeight
+}
