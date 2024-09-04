4c4ec8db9fe5bcca628531d07f0a082864401a31
Cao-nguyen <chinhphucnguvan@gmail.com>
2024-09-04 12:54:42 +0700

    o

--- controllers/client/chatControllers.js
+++ controllers/client/chatControllers.js
@@ -1,20 +1,41 @@
 const Chat = require('../../models/chatModel')
+const User = require('../../models/usersModel')
 
 // [GET] /chat
 module.exports.index = async (req, res) => {
     const userId = res.locals.user._id
+    const fullName = res.locals.user.fullName
 
-    _io.on('connection', (socket) => {
+    _io.once('connection', (socket) => {
         socket.on('CLIENT_SEND_MESSAGE', async (content) => {
             const chat = new Chat({
                 user_id: userId,
                 content: content,
             })
             await chat.save()
+
+            _io.emit("SERVER_RETURN_MESSAGE", {
+                userId: userId,
+                fullName: fullName,
+                content: content
+            })
         })
     })
 
+    const chats = await Chat.find({
+        deleted: false
+    })
+
+    for (const chat of chats) {
+        const inforUser = await User.findOne({
+            _id: chat.user_id 
+        }).select("fullName")
+
+        chat.inforUser = inforUser
+    }
+
     res.render('client/pages/chat/index', {
-        pageTitle: 'Chat'
+        pageTitle: 'Chat',
+        chats: chats
     })
 }
--- public/js/chat.js
+++ public/js/chat.js
@@ -1,3 +1,4 @@
+// CLIENT_SEND_MESSAGE
 const formSendData = document.querySelector(".messages .message-input");
 
 if (formSendData) {
@@ -11,3 +12,17 @@ if (formSendData) {
         }
     });
 }
+
+// SERVER_RETURN_MESSAGE
+socket.on("SERVER_RETURN_MESSAGE", (data) => {
+    const body = document.querySelector(".chat-area .messages")
+
+    const div = document.createElement("div") 
+    div.classList.add("message.received")
+    div.innerHTML = `
+        <div class="message-content">${data.fullName}</div>
+        <div class="message-chat">${data.content}</div>
+    `
+
+    body.appendChild(div)
+})
